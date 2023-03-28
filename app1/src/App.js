952b51f29074ccb33c6ec6691864cb5580f1c218
Bourbonjosh <Bourbonjosh@ninefingers>
2023-03-28 17:04:30 +0200

    Exercises 2.1 - 2.3 done

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,5 +1,5 @@
 const Header = (props) => {
-  return <h1>{props.course}</h1>
+  return <h1>{props.name}</h1>
 }
 
 const Part = (props) => {
@@ -8,53 +8,73 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  console.log('Content props', props);
   return (
     <>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-    
+    {props.parts.map(part =>
+      <Part key={part.id} part={part} />
+    )}
     </>
-  );
+  )
 }
 
 const Total = (props)=> {
   return (
     <>
-    <p>Number of exercises {props.total}</p>
+    <h3>Total of {props.total} exercises</h3>
     </>
   )
 }
 
-
+const Course = (props) => {
+  const total = props.course.parts.reduce((s,p) => s + p.exercises, 0);
+  return (
+    <div>
+      <Header name={props.course.name}/>
+      <Content parts={props.course.parts} />
+      <Total total={total} />
+    </div>
+  )
+}
 
 const App = () => {
 
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course.name}/>      
-      <Content parts={course.parts} />
-      <Total total={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises} />
-    </div>
-  )
+  return <Course course={course} />
+
+  // return (
+  //   <div>
+  //     <Header name={course.name}/>      
+  //     <Content parts={course.parts} />
+  //     <Total total={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises} />
+  //   </div>
+  // )
 }
 
 export default App;
