1a4515dcb3086f60ddb8b564b8220c16b63adf87
Bourbonjosh <Bourbonjosh@Icarum>
2023-03-29 16:51:59 +0200

    Exercise 2.9 done

--- app2/src/App.js
+++ app2/src/App.js
@@ -6,15 +6,18 @@ const App = () => {
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filterName, setFilterName] = useState('');
+
+  const filteredPersons = persons.filter(p => p.name.toLowerCase().includes(filterName.toLowerCase()));
 
   const addPerson = (event) => {
     event.preventDefault();
-    const isNameUnique = persons.every(p => p.name !== newName)
+    const isNameUnique = persons.every(p => p.name !== newName);
     if (!isNameUnique) {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${newName} is already added to phonebook`);
     }
     else {
       const nameObject = {
@@ -28,6 +31,10 @@ const App = () => {
     }
   }
 
+  const handleFilterNameChange =(event) => {
+    setFilterName(event.target.value);
+  }
+
   const handleNameChange =(event) => {
     setNewName(event.target.value);
   }
@@ -39,6 +46,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with: <input value={filterName} onChange={handleFilterNameChange} /></div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
@@ -49,7 +58,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         <ul>
-          {persons.map(p =>
+          {filteredPersons.map(p =>
             <li key={p.id}>{p.name} {p.number}</li>
           )}
         </ul>
