bfa33de383662420a3726bfd7b7cc1a987dc758b
Bourbonjosh <Bourbonjosh@Icarum>
2023-05-04 16:45:30 +0200

    2.15 with fetch - 1

--- app2/src/services/persons.js
+++ app2/src/services/persons.js
@@ -3,13 +3,13 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/persons'
 
 
-const getAll = () => axios
+/*const getAll = () => axios
                        .get(baseUrl)
-                       .then(response => response.data);
+                       .then(response => response.data);*/
                       
-const create = newObject => axios
+/*const create = newObject => axios
                               .post(baseUrl, newObject)
-                              .then(response => response.data);
+                              .then(response => response.data);*/
 
 const remove = id => axios
                        .delete(`${baseUrl}/${id}`)
@@ -19,5 +19,22 @@ const remove = id => axios
 const update = (id, newObject) => axios
                                     .put(`${baseUrl}/${id}`, newObject)
                                     .then(response => response.data);
+
+// And that's how you do it using fetch() instead of axios
+// -------------------------------------------------------
+
+const getAll = () => fetch(baseUrl)
+                        .then(async response => await response.json());
+
+const create = newObject => fetch(baseUrl, {
+                                method: "POST",
+                                body: newObject,
+                                headers: {
+                                    'Accept': 'application/json',
+                                    'Content-Type': 'application/json'
+                                }
+                            })
+                                .then(async response => await response.json());
+
 
 export default {getAll, create, remove, update}
